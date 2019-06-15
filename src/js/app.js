a9ed3788a0d8a58e67ede9289971557591a0be35
Eveline <m26416041@john.petra.ac.id>
2019-06-16 02:10:32 +0700

    FINISH

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register('sw.js')
+navigator.serviceWorker.register('/~m26416041/movie/sw.js');
 
 var deferredPrompt;
 window.addEventListener('beforeinstallprompt', function(event) {
@@ -7,13 +7,3 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
-
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//         navigator.serviceWorker.register('https://opensource.petra.ac.id/~m26416041/movie/sw.js').then(function(registration) {
-//           console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//           console.log('ServiceWorker registration failed: ', err);
-//           });
-//       });
-// }
--- sw.js
+++ sw.js
@@ -1,262 +1,59 @@
 self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
     caches.open('first-app')
       .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
-          '/',
-          'index.php',
-          'details.php',          
-          'styles.css',
-          'src/js/app.js',
+          '/~m26416041/movie/',
+          '/~m26416041/movie/index.html',
+          '/~m26416041/movie/details.html',
+          '/~m26416041/movie/styles.css',
+          '/~m26416041/movie/src/js/app.js',
           'https://code.jquery.com/jquery-3.3.1.slim.min.js',
           'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en',
-          'https://code.getmdl.io/1.3.0/material.grey-orange.min.css',
-          'https://code.getmdl.io/1.3.0/material.min.js',
-          'images/192.png',
-          'images/152.png'
+          'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en'
         ])
       })
   );
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        return res;
-      })
-  );
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
 });
 
 
-// // Cache
+self.addEventListener('fetch', event => {
+  // Prevent the default, and handle the request ourselves.
+  event.respondWith(async function() {
+    // Try to get the response from a cache.
+    const cachedResponse = await caches.match(event.request);
+    // Return it if we found one.
+    if (cachedResponse) return cachedResponse;
+    // If we didn't find a match in the cache, use the network.
+    return fetch(event.request);
+  }());
+});
+
+
 // self.addEventListener('install', function(event) {
+//   console.log('[Service Worker] Installing Service Worker ...', event);
 //   event.waitUntil(
-//     caches.open('testtok')
+//     caches.open('static')
 //       .then(function(cache) {
-//         cache.add('/app.js')
+//         console.log('[Service Worker] Precaching App Shell');
+//         cache.add('/~m26416041/movie/src/js/app.js')
 //       })
-//   );
+//   )
 // });
 
 // self.addEventListener('activate', function(event) {
+//   console.log('[Service Worker] Activating Service Worker ....', event);
 //   return self.clients.claim();
 // });
 
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(fetch(event.request));
-// });
-
-// // Install
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open('static')
-//       .then(function(cache) {
-//         console.log('[Service Worker] Precaching App Shell');
-//         cache.addAll([
-//           '/../../',
-//           '/../../index.php',
-//           '/../../styles.css',
-//           'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-//           'https://fonts.googleapis.com/icon?family=Material+Icons',
-//           'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en',
-//           'https://code.getmdl.io/1.3.0/material.grey-orange.min.css',
-//           'https://code.getmdl.io/1.3.0/material.min.js',
-//           '/../../images/192.png',
-//           '/../../images/152.png'
-//         ]);
-//       })
-//   )
-// });
-
-// // Delete Old Cache
-// self.addEventListener('activate', function(event) {
-//   console.log('[Service Worker] Activating Service Worker ....', event);
-//   event.waitUntil(
-//     caches.keys()
-//       .then(function(keyList) {
-//         return Promise.all(keyList.map(function(key) {
-//           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-//             console.log('[Service Worker] Removing old cache.', key);
-//             return caches.delete(key);
-//           }
-//         }));
-//       })
-//   );
-//   return self.clients.claim();
-// });
-
-// // Providing an offline fallback page
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         } else {
-//           return fetch(event.request)
-//             .then(function(res) {
-//               return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//             })
-//             .catch(function(err) {
-//               return caches.open(CACHE_STATIC_NAME)
-//                 .then(function(cache) {
-//                   return cache.match('/offline.html');
-//                 });
-//             });
-//         }
-//       })
-//   );
-// });
-
-// // Cache then Network Strategy
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.open(CACHE_DYNAMIC_NAME)
-//       .then(function(cache) {
-//         return fetch(event.request)
-//           .then(function(res) {
-//             cache.put(event.request, res.clone());
-//             return res;
-//           });
-//       })
-//   );
-// });
-
-
-
-
-
-
-// self.addEventListener('activate', function(event) {
-//   console.log('[Service Worker] Activating Service Worker ....', event);
-//   event.waitUntil(
-//     caches.keys()
-//       .then(function(keyList) {
-//         return Promise.all(keyList.map(function(key) {
-//           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-//             console.log('[Service Worker] Removing old cache.', key);
-//             return caches.delete(key);
-//           }
-//         }));
-//       })
-//   );
-//   return self.clients.claim();
-// });
-
-// //Cache then network strategy
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     event.respondWith(
-//     caches.open(CACHE_DYNAMIC_NAME)
-//       .then(function(cache) {
-//         return fetch(event.request)
-//           .then(function(res) {
-//             cache.put(event.request, res.clone());
-//             return res;
-//           });
-//       })
-//   );
-// });
-
-// // network with cache fallback
-//   self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     event.respondWith(
-//     fetch(event.request)
-//       .then(function(res) {
-//         return caches.open(CACHE_DYNAMIC_NAME)
-//                 .then(function(cache) {
-//                   cache.put(event.request.url, res.clone());
-//                   return res;
-//                 })
-//       })
-//       .catch(function(err) {
-//         return caches.match(event.request);
-//       })
-
-//   );
-// });
-
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(res) {
-//         return res;
-//       })
-//   );
-// });
-
-
-
-// self.addEventListener('install', event => {
-//  event.waitUntil(
-//    caches.open($CACHE_STORE)
-//      .then(function (cache) {
-//        cache.addAll($FILES_ADDITIONAL);
-//        return cache.addAll($FILES_IMPORTANT);
-//      })
-//      .then(() => {
-//        return self.skipWaiting();
-//      })
-//  );
-// });
-
-// self.addEventListener('activate', event => {
-//   event.waitUntil(
-//     caches.open($CACHE_STORE)
-//       .then(cache => {
-//         return cache.keys()
-//           .then(cacheNames => {
-//             return Promise.all(
-//               cacheNames.filter(cacheName => {
-//                 return $FILES.indexOf(cacheName) === -1;
-//               }).map(cacheName => {
-//                 return caches.delete(cacheName);
-//               })
-//             );
-//           })
-//           .then(() => {
-//             return self.clients.claim();
-//           });
-//       })
-//   );
-// }); 
-
-// self.addEventListener('fetch', event => {
-//   if (event.request.method === 'GET') {
-//     let url = event.request.url.indexOf(self.location.origin) !== -1 ?
-//       event.request.url.split(`${self.location.origin}/`)[1] :
-//       event.request.url;
-//     let isFileCached = $FILES.indexOf(url) !== -1;
-
-//     if(!isFileCached && event.request.mode === 'navigate' && $NAVIGATION_FALLBACK){
-//      url = $NAVIGATION_FALLBACK;
-//      isFileCached = $FILES.indexOf(url) !== -1;
-//     }
-
-//     if (isFileCached) {
-//       event.respondWith(
-//         caches.open($CACHE_STORE)
-//           .then(cache => {
-//             return cache.match(url)
-//               .then(response => {
-//                 if (response) {
-//                   return response;
-//                 }
-//                 throw Error('There is not response for such request', url);
-//               });
-//           })
-//           .catch(error => {
-//             return fetch(event.request);
-//           })
-//       );
-//     }
-//   }
 // });
